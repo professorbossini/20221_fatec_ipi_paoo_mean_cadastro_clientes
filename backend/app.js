0bf90d14acc97df8ca47abaaa29522a19417e0fb
Rodrigo Bossini <professorbossini@gmail.com>
2022-06-01 16:23:17 -0300

    apostila04: fim da apostila
    GET e POST funcionando junto ao MongoDB. dotenv configurado

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,26 @@
+require('dotenv').config()
 const express = require ('express')
 const cors = require ('cors')
 const app = express()
+const mongoose = require('mongoose')
+const Cliente = require ("./models/cliente")
+
+const {
+    MONGODB_USER,
+    MONGODB_PASSWORD,
+    MONGODB_HOST,
+    MONGODB_CLUSTER,
+    MONGODB_DATABASE
+} = process.env
+
+mongoose.connect(`mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_CLUSTER}.${MONGODB_HOST}.mongodb.net/${MONGODB_DATABASE}?retryWrites=true&w=majority`)
+.then(() => {
+    console.log("Conexão OK")
+})
+.catch((e) => {
+    console.log("Conexão NOK" + e)
+})
+
 app.use(cors())
 //req.body pode ser tratado como um objeto JSON
 app.use(express.json())
@@ -21,15 +41,26 @@ const clientes = [
 
 //localhost:3000/api/clientes
 app.get('/api/clientes', (req, res) => {
-    res.status(200).json({
-        mensagem: "Tudo OK",
-        clientes: clientes
+    Cliente.find().then(documents => {
+        res.status(200).json({
+            mensagem: "Tudo OK",
+            clientes: documents
+        })
     })
 })
 
 //localhost:3000/api/clientes
 app.post('/api/clientes', (req, res) => {
-    console.log(req.body)
+    //construir um objeto do tipo Cliente com os dados vindos da requisição
+    const cliente = new Cliente({
+        nome: req.body.nome,
+        fone: req.body.fone,
+        email: req.body.email
+    })
+    //exibir com um log
+    console.log(cliente)
+    //armazenar no mongodb
+    cliente.save()
     res.status(201).json({mensagem: 'Cliente inserido'})
 })
 
